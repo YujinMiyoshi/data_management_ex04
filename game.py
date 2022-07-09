8ee259a64bdc7eb13f79c73b095b52e690dc0b93
EugeneSatya <ecommerceproject389@gmail.com>
2022-07-10 03:38:02 +0900

    HoT: initial revision

---
+++ game.py
@@ -0,0 +1,18 @@
+import random
+round = 0
+coin = random.randint(1,2)
+h = 0
+t = 0
+
+print("Tossing a coin...")
+while round < 3:
+    coin = random.randint(1,2)
+    round += 1
+    if coin == 1:
+        print("Round", round,": Heads")
+        h += 1
+    elif coin == 2:
+        print("Round", round,": Tails")
+        t += 1
+print("Heads:", h,", Tails:", t)
+
